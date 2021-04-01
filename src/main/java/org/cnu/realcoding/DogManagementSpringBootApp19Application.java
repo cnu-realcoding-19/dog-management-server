f52a79a843c108a810018759c9f832cfb414da4e
Kim-AYoung <onionlily123@naver.com>
2021-04-02 00:13:52 +0900

    setup spring boot app

---
+++ src/main/java/org/cnu/realcoding/DogManagementSpringBootApp19Application.java
@@ -0,0 +1,13 @@
+package org.cnu.realcoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogManagementSpringBootApp19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogManagementSpringBootApp19Application.class, args);
+	}
+
+}
