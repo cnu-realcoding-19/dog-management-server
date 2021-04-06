6d28078731a87469df6187d79bfc6d22662e9aee
Kim-AYoung <onionlily123@naver.com>
2021-04-06 13:25:59 +0900

    한 파라미터를 조회할 떄, 여러dog 존재가능성으로 수정

--- src/main/java/org/cnu/realcoding/repository/DogRepository.java
+++ src/main/java/org/cnu/realcoding/repository/DogRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class DogRepository {
     @Autowired
@@ -16,16 +18,16 @@ public class DogRepository {
         return mongoTemplate.findOne(Query.query(Criteria.where("name").is(name).and("ownerName").is(ownerName).and("ownerPhoneNumber").is(ownerPhoneNumber)), Dog.class);
     }
 
-    public Dog findDogByName(String name){
-        return mongoTemplate.findOne(Query.query(Criteria.where("name").is(name)), Dog.class);
+    public List<Dog> findDogsByName(String name){
+        return mongoTemplate.find(Query.query(Criteria.where("name").is(name)), Dog.class);
     }
 
-    public Dog findDogByOwnerName(String ownerName){
-        return mongoTemplate.findOne(Query.query(Criteria.where("ownerName").is(ownerName)), Dog.class);
+    public List<Dog> findDogsByOwnerName(String ownerName){
+        return mongoTemplate.find(Query.query(Criteria.where("ownerName").is(ownerName)), Dog.class);
     }
 
-    public Dog findDogByOwnerPhoneNumber(String ownerPhoneNumber){
-        return mongoTemplate.findOne(Query.query(Criteria.where("ownerPhoneNumber").is(ownerPhoneNumber)), Dog.class);
+    public List<Dog> findDogsByOwnerPhoneNumber(String ownerPhoneNumber){
+        return mongoTemplate.find(Query.query(Criteria.where("ownerPhoneNumber").is(ownerPhoneNumber)), Dog.class);
     }
 
     public void insertDog(Dog dog) {
