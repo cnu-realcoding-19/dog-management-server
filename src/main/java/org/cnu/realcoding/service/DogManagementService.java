96afdc8851baad95e35c7bc72bcf83914b4dea4f
Kim-AYoung <onionlily123@naver.com>
2021-04-06 13:13:07 +0900

    나머지 조건으로 기존에 등록된 강아지 조회하는 api구현

--- src/main/java/org/cnu/realcoding/service/DogManagementService.java
+++ src/main/java/org/cnu/realcoding/service/DogManagementService.java
@@ -15,12 +15,37 @@ public class DogManagementService {
     public void insertDog(Dog dog) {
         if (dogRepository.findDog(dog.getName(),dog.getOwnerName(),dog.getOwnerPhoneNumber()) == null){
             dogRepository.insertDog(dog);
+        }else {
+            throw new DogConflictException();
         }
-        throw new DogConflictException();
     }
 
     public Dog getDogByName(String name) {
-        Dog dog = dogRepository.findDog(name);
+        Dog dog = dogRepository.findDogByName(name);
+        if (dog==null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+    public Dog getDogByOwnerName(String ownerName) {
+        Dog dog = dogRepository.findDogByOwnerName(ownerName);
+        if (dog==null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+    public Dog getDogByOwnerPhoneNumber(String ownerPhoneNumber) {
+        Dog dog = dogRepository.findDogByOwnerPhoneNumber(ownerPhoneNumber);
+        if (dog==null){
+            throw new DogNotFoundException();
+        }
+        return dog;
+    }
+
+    public Dog getDog(String name, String ownerName, String ownerPhoneNumber) {
+        Dog dog = dogRepository.findDog(name, ownerName, ownerPhoneNumber);
         if (dog==null){
             throw new DogNotFoundException();
         }
